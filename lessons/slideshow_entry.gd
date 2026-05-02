19fb78cff312d1c2600556dd9ba6ef5fd5ce1dfa
JackL <jrlorinc@icloud.com>
2026-05-02 12:03:21 -0400

    buggy resources

---
+++ lessons/slideshow_entry.gd
@@ -0,0 +1,6 @@
+class_name SlideShowEntry extends Resource
+
+@export var expression := preload("res://assets/emotion_regular.png")
+@export var character := preload("res://assets/sophia.png")
+@export_multiline var text := ""
+@export var voice := preload("res://assets/talking_synth.ogg")
